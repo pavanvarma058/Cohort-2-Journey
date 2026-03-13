a4bbdc5406ff91dd172c337b41ddb613d9a01125
Pavan Varma <pavanvarma058@gmail.com>
2026-03-13 17:19:30 +0000

    Learned about the memo in react which helps in rerendering process

--- Week-06/week-6.1/week-6-experiments/src/App.jsx
+++ Week-06/week-6.1/week-6-experiments/src/App.jsx
@@ -7,16 +7,38 @@ function App() {
     setTitle("My name is " + Math.random());
   }
   return (
-    <>
+    <div>
       <button onClick={changeTitle}>Click here to change title</button>
-      <Header title={firstTitle} />
-      <Header title="Hello React" />
-    </>
+      <Header2 title={firstTitle}></Header2>
+      {/* <HeaderWithButton /> */}
+      <Header2 title="Hello React"></Header2>
+      <Header2 title="Hello React"></Header2>
+      <Header2 title="Hello React"></Header2>
+      <Header2 title="Hello React"></Header2>
+    </div>
   );
 }
+
+// function HeaderWithButton() {
+//   const [firstTitle, setTitle] = React.useState("My name is Yeswanth");
+
+//   function changeTitle() {
+//     setTitle("My name is " + Math.random());
+//   }
+//   return (
+//     <>
+//       <button onClick={changeTitle}>Click here to change title</button>
+//       <Header title={firstTitle} />
+//     </>
+//   );
+// }
 
 function Header({ title }) {
   return <div>{title}</div>;
 }
+
+const Header2 = React.memo(({ title }) => {
+  return <div>{title}</div>;
+});
 
 export default App;
